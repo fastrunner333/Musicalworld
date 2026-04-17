b172bbd1abd26e8954aa654186b4c68627da8800
musical <44aayushaa@gmail.com>
2026-04-18 00:43:26 +0530

    mostly fixed

--- src/Userpage/Userpost.jsx
+++ src/Userpage/Userpost.jsx
@@ -68,7 +68,7 @@ export function Userpost(){
                            }  
                        })
                .catch(err=>console.log(err))
-               },[userToken])        
+               },[])        
       
        
        
