76c72c082411127631f6679102ac4f439b289311
musical <44aayushaa@gmail.com>
2026-04-18 01:44:12 +0530

    final

--- src/Header/User.jsx
+++ src/Header/User.jsx
@@ -1,15 +1,19 @@
 import {useNavigate} from "react-router"
 import {USER} from "../Context/Usercontext"
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 
 export function User(){
     const {userToken} = useContext(USER) 
     const navigate = useNavigate()
+    const [username,setusername] = useState(userToken)
+    useEffect(()=>{
+        setusername(userToken)
+    },[userToken])
 
     return(
         <div style={{display:"flex", width:"17%"}}>
             
-            <img style={{width:"7vw",height:"stretch",maxHeight:"7vh",maxWidth:"5vw"}} alt="Image"src={`https://res.cloudinary.com/dg9zjuhjn/image/upload/userpict/${userToken}userpic.png?c=${Date.now()}`} />
+            <img style={{width:"7vw",height:"stretch",maxHeight:"7vh",maxWidth:"5vw"}} alt="Image"src={`https://res.cloudinary.com/dg9zjuhjn/image/upload/userpict/${username}userpic.png?c=${Date.now()}`} />
             <button style={{alignSelf:"center",border:"none",color:"#f4e3b2",backgroundColor:"#45462a",fontFamily:"Georgia, 'Times New Roman', Times, serif",fontSize:"large"}} onClick={()=>navigate("/userinfo")}>{userToken}</button>
         </div>
     )
--- src/Security/Protectedroutes.jsx
+++ src/Security/Protectedroutes.jsx
@@ -22,10 +22,9 @@ export function ProtectedRoute({children}){
         })
         .then((data)=>{
             setuserToken(data)
-            return children
         })
         
-    
+    return children
        
     
 }
