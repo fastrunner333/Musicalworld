5a57cb54fde820a78e16f5cd6675142499cc9ba8
musical <44aayushaa@gmail.com>
2026-05-04 00:35:24 +0530

    feed sorted

--- src/Content/Post.jsx
+++ src/Content/Post.jsx
@@ -13,20 +13,8 @@ export function Post({filter}){
         .then((unsorteddata)=>{
                             let sorteddata = []
                             if(unsorteddata.data && unsorteddata.data.length > 0){
-                                sorteddata = unsorteddata.data
-                                const len = sorteddata.length
-                                for(let i=0;i<len-1;i++){
-                                    for(let j=0;j<len-1;j++){
-                                        console.log(sorteddata[j].uploaddate," out")
-                                        if(sorteddata[j].uploaddate < sorteddata[j+1].uploaddate){
-                                            console.log(sorteddata[j].uploaddate," if top")
-                                            let temp = sorteddata[j]
-                                            sorteddata[j] = sorteddata[j+1]
-                                            sorteddata[j+1] = temp
-                                            console.log(sorteddata[j].uploaddate," if bottom")
-                                        }
-                                    }
-                                }
+                                sorteddata = [...unsorteddata.data].sort((a,b)=>b.uploaddate - a.uploaddate)
+                                
                             }
                             console.log(sorteddata)
                             return sorteddata
