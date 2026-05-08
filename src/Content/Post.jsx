8bf61483e72632804c5b1366e932030a1185fffc
musical <44aayushaa@gmail.com>
2026-05-08 19:20:17 +0530

    like and dislike added

--- src/Content/Post.jsx
+++ src/Content/Post.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react"
 import styles from "./Post.module.css"
-import {USER} from "../Context/Usercontext";
+//import {USER} from "../Context/Usercontext";
 
 
 
@@ -10,7 +10,7 @@ export function Post({filter}){
     const [likes, setlikes] = useState(0)
     const [postid, setpostid] = useState("")
     const [dislikes, setdislikes] =useState(0)
-    const {userToken} = useContext(USER)
+    //const {userToken} = useContext(USER)
 
     function senddislike(){/* 
         fetch(`https://musicalworld.onrender.com/dislike`),{
