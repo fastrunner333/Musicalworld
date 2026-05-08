d454f09e28f2a8c2b18d94ca64a67afb759cae8b
musical <44aayushaa@gmail.com>
2026-05-09 02:20:49 +0530

    like and dislike added

--- src/Content/Post.jsx
+++ src/Content/Post.jsx
@@ -8,11 +8,17 @@ export function Post({filter}){
     
     const [result, setresult] = useState([])
     const {userToken} = useContext(USER)
+    let dataarr = []
+    let arr = []
     let updater = 0
     let likes = 0
     let dislikes = 0 
+    let buttoncsslike = ""
+    let clicklogiclike = null
+    let buttoncssdislike = ""
+    let clicklogicdislike = null
 
-    const senddislike = (e)=>{
+    const senddislike = (index, e)=>{
             const id = e.currentTarget.dataset.id
             fetch("https://musicalworld.onrender.com/dislike",{
             method:"POST",
@@ -27,7 +33,7 @@ export function Post({filter}){
         .catch(err=>console.log(err))  
     }
     
-    const sendlike = (e)=>{
+    const sendlike = (index, e)=>{
         const id = e.currentTarget.dataset.id
         fetch("https://musicalworld.onrender.com/like",{
             method:"POST",
@@ -40,6 +46,82 @@ export function Post({filter}){
         .then(res=>res.json())
         .then(txt=>console.log(txt))
         .catch(err=>console.log(err))  
+        //arr[index]
+
+    }
+
+    function setdata(data){
+        arr = (data.map((post, index)=>{   
+                                            likes = post.likes
+                                            dislikes = post.dislikes
+                                            let i = index
+                                                        
+                                            if(!post.mediatype){
+                                                if(post.liked){
+                                                    buttoncsslike = styles.likebuttongrey
+                                                    clicklogiclike = null
+                                                }
+                                                else{
+                                                    buttoncsslike = styles.likebutton
+                                                    clicklogiclike = sendlike
+                                                }
+                                                if(post.disliked){
+                                                    buttoncssdislike = styles.dislikebuttongrey
+                                                    clicklogicdislike = null
+                                                }
+                                                else{
+                                                    buttoncssdislike = styles.dislikebutton
+                                                    clicklogicdislike = senddislike
+                                                }
+                                            return  <div key={index} className={styles.userpostnomedia}>
+                                                    <div className={styles.title}>{post.posttitle}</div>
+                                                    <div className={styles.likedislike}>
+
+                                                        <button data-id={post._id} className={buttoncsslike} onClick={clicklogiclike(i,e)}></button>
+                                                        <span className={styles.count}>{likes}</span>
+
+                                                        <button data-id={post._id} className={buttoncssdislike} onClick={clicklogicdislike(i,e)}></button>
+                                                        <span className={styles.count}>{dislikes}</span>
+
+                                                    </div>
+                                                    <div className={styles.user}>{post.username}</div>
+                                                    </div>
+                                                }
+                                            else if(post.mediatype == ".png" || post.mediatype == ".jpg" || post.mediatype == ".jpeg"){
+                                                return  <div key={index}  className={styles.userpostimage}>
+                                                        <div className={styles.title}>{post.posttitle}</div>
+                                                        <img className={styles.img} src={`https://res.cloudinary.com/dg9zjuhjn/image/upload/useruploads/${post.username}-${post.uploaddate}${post.mediatype}`} />
+                                                        <div className={styles.user}>{post.username}</div>
+                                                        </div>
+                                                }
+                                            else if(post.mediatype == ".mp3"){
+                                                return  <div key={index}  className={styles.userpostaudio}>
+                                                        <div className={styles.title}>{post.posttitle}</div>
+                                                        <audio className={styles.audio} controls src={`https://res.cloudinary.com/dg9zjuhjn/video/upload/useruploads/${post.username}-${post.uploaddate}${post.mediatype}`} type="audio/mpeg"></audio>
+                                                        <div className={styles.user}>{post.username}</div>
+                                                        </div>
+                                                        }
+                                            else if(post.mediatype == ".m4a"){
+                                                return  <div  key={index} className={styles.userpostaudio}>
+                                                        <div className={styles.title}>{post.posttitle}</div>
+                                                        <audio className={styles.audio} controls src={`https://res.cloudinary.com/dg9zjuhjn/video/upload/useruploads/${post.username}-${post.uploaddate}${post.mediatype}`} type="audio/x-m4a"></audio>
+                                                        <div className={styles.user}>{post.username}</div>
+                                                        </div>
+                                                }
+                                            else if(post.mediatype == ".mp4"){
+                                                return  <div  key={index} className={styles.userpostvideo}>
+                                                        <div className={styles.title}>{post.posttitle}</div>
+                                                        <video className={styles.video} controls src={`https://res.cloudinary.com/dg9zjuhjn/video/upload/useruploads/${post.username}-${post.uploaddate}${post.mediatype}`} type="video/mp4"></video>
+                                                        <div className={styles.user}>{post.username}</div>
+                                                        </div>
+                                                }
+                                            else{
+                                                return <div>Error</div>
+                                                }
+                                                            
+                                            }))
+        console.log(arr)
+        setresult(arr)
     }
    
     useEffect(()=>{
@@ -54,85 +136,14 @@ export function Post({filter}){
                                 
                             }
                             console.log(sorteddata)
+                            dataarr = sorteddata
                             return sorteddata
                             }
         )
         .then((data)=>{
             if(data!=[]){
-                    setresult(data.map((post, index)=>{  
-                                                        
-                                                        likes = post.likes
-                                                        dislikes = post.dislikes
-                                                        let buttoncsslike = ""
-                                                        let clicklogiclike = null
-                                                        let buttoncssdislike = ""
-                                                        let clicklogicdislike = null
-                                                        
-                                                        if(!post.mediatype){
-                                                            if(post.liked){
-                                                                buttoncsslike = styles.likebuttongrey
-                                                                clicklogiclike = null
-                                                            }
-                                                            else{
-                                                                buttoncsslike = styles.likebutton
-                                                                clicklogiclike = sendlike
-                                                            }
-                                                            if(post.disliked){
-                                                                buttoncssdislike = styles.dislikebuttongrey
-                                                                clicklogicdislike = null
-                                                            }
-                                                            else{
-                                                                buttoncssdislike = styles.dislikebutton
-                                                                clicklogicdislike = senddislike
-                                                            }
-                                                        return  <div key={index} className={styles.userpostnomedia}>
-                                                                <div className={styles.title}>{post.posttitle}</div>
-                                                                <div className={styles.likedislike}>
-
-                                                                    <button data-id={post._id} className={buttoncsslike} onClick={clicklogiclike}></button>
-                                                                    <span className={styles.count}>{likes}</span>
-
-                                                                    <button data-id={post._id} className={buttoncssdislike} onClick={clicklogicdislike}></button>
-                                                                    <span className={styles.count}>{dislikes}</span>
-
-                                                                </div>
-                                                                <div className={styles.user}>{post.username}</div>
-                                                                </div>
-                                                            }
-                                                        else if(post.mediatype == ".png" || post.mediatype == ".jpg" || post.mediatype == ".jpeg"){
-                                                            return  <div key={index}  className={styles.userpostimage}>
-                                                                    <div className={styles.title}>{post.posttitle}</div>
-                                                                    <img className={styles.img} src={`https://res.cloudinary.com/dg9zjuhjn/image/upload/useruploads/${post.username}-${post.uploaddate}${post.mediatype}`} />
-                                                                    <div className={styles.user}>{post.username}</div>
-                                                                    </div>
-                                                            }
-                                                        else if(post.mediatype == ".mp3"){
-                                                            return  <div key={index}  className={styles.userpostaudio}>
-                                                                    <div className={styles.title}>{post.posttitle}</div>
-                                                                    <audio className={styles.audio} controls src={`https://res.cloudinary.com/dg9zjuhjn/video/upload/useruploads/${post.username}-${post.uploaddate}${post.mediatype}`} type="audio/mpeg"></audio>
-                                                                    <div className={styles.user}>{post.username}</div>
-                                                                    </div>
-                                                                    }
-                                                        else if(post.mediatype == ".m4a"){
-                                                            return  <div  key={index} className={styles.userpostaudio}>
-                                                                    <div className={styles.title}>{post.posttitle}</div>
-                                                                    <audio className={styles.audio} controls src={`https://res.cloudinary.com/dg9zjuhjn/video/upload/useruploads/${post.username}-${post.uploaddate}${post.mediatype}`} type="audio/x-m4a"></audio>
-                                                                    <div className={styles.user}>{post.username}</div>
-                                                                    </div>
-                                                            }
-                                                        else if(post.mediatype == ".mp4"){
-                                                            return  <div  key={index} className={styles.userpostvideo}>
-                                                                    <div className={styles.title}>{post.posttitle}</div>
-                                                                    <video className={styles.video} controls src={`https://res.cloudinary.com/dg9zjuhjn/video/upload/useruploads/${post.username}-${post.uploaddate}${post.mediatype}`} type="video/mp4"></video>
-                                                                    <div className={styles.user}>{post.username}</div>
-                                                                    </div>
-                                                            }
-                                                        else{
-                                                            return <div>Error</div>
-                                                            }
-                                                            
-                                                        }))
-                            }  
+                setdata(data)
+            }  
                     }
             )
             .catch(err=>console.log(err))
