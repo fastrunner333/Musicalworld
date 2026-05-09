a41cd6e1ed375dd21c2ed8e85c393058cfedf3fe
musical <44aayushaa@gmail.com>
2026-05-09 13:29:50 +0530

    likes

--- src/Security/Protectedroutes.jsx
+++ src/Security/Protectedroutes.jsx
@@ -8,25 +8,23 @@ export async function ProtectedRoute({children}){
     const navigate = useNavigate()
     const {setuserToken} = useContext(USER)
     const {load, setload} = useState(true)
-    useEffect(()=>{
-        fetch("https://musicalworld.onrender.com/check",{   
-            method:"POST", 
-            credentials:"include"
-        })
-        .then((res)=>{
-            if(res.status != 200){
-                navigate("/login")
-            }
-            else{
-                setload(false)
-                return res.text()
-            }      
-        })
-        .then((data)=>{
-            setuserToken(data)
-        })
-        .catch(()=>navigate("/login"))
-    },[])
+    fetch("https://musicalworld.onrender.com/check",{   
+        method:"POST", 
+        credentials:"include"
+    })
+    .then((res)=>{
+        if(res.status != 200){
+            navigate("/login")
+        }
+        else{
+            setload(false)
+            return res.text()
+        }      
+    })
+    .then((data)=>{
+        setuserToken(data)
+    })
+    .catch(()=>navigate("/login"))
 
     if(load){
         return <div>Loading please wait as free servers are slow......</div>
