591e0abd820134e50edd3b095c6b8caba6ad5853
musical <44aayushaa@gmail.com>
2026-05-09 12:55:42 +0530

    likes

--- src/Security/Protectedroutes.jsx
+++ src/Security/Protectedroutes.jsx
@@ -1,13 +1,13 @@
 import {useNavigate} from "react-router"
 import{USER} from "../Context/Usercontext"
-import { useContext } from "react"
+import { useContext, useEffect } from "react"
 import { Mainpage } from "../Mainpage/Mainpage"
 
 export function ProtectedRoute({children}){
 
     const navigate = useNavigate()
     const {setuserToken} = useContext(USER)
-    
+    useEffect(()=>{
         fetch("https://musicalworld.onrender.com/check",{   
             method:"POST", 
             credentials:"include"
@@ -15,15 +15,13 @@ export function ProtectedRoute({children}){
         .then((res)=>{
             if(res.status != 200){
                 navigate("/login")
-                return
             }
             else{
                 setuserToken(res.text())
                 return children
             }      
         })
-        
-    return
+    },[])
        
     
 }
