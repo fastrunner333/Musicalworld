745bdc703b6bdbe8fa109ab406707832cc04a789
musical <44aayushaa@gmail.com>
2026-05-09 18:21:15 +0530

    likes

--- src/Loginsignup/Loginsignup.jsx
+++ src/Loginsignup/Loginsignup.jsx
@@ -101,7 +101,10 @@ export function Loginsignup(){
                     </button>
                 </div>
             </form>
-            <div className={styles.disclamer}>This website has option for making accounts disabled, to use the website use the provided username and password, this website is not for use, you are prohibited from making account on this website</div>
+            <div className={styles.disclamer}>
+            <div style={{color:"blue"}}>Servers are slow please be patient, website is not crashing, first request usually takes long, <bold>refresh</bold> if taking too long</div>
+            This website has option for making accounts disabled, to use the website use the provided username and password, this website is not for use, you are prohibited from making account on this website
+            </div>
         </>
     )
 }
