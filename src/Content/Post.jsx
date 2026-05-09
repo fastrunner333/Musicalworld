9a051a75fb35152aff02927e1ce7840707f5fcd1
musical <44aayushaa@gmail.com>
2026-05-09 10:50:13 +0530

    likes

--- src/Content/Post.jsx
+++ src/Content/Post.jsx
@@ -55,9 +55,9 @@ export function Post({filter}){
         const spanlike = spanlikeref.current.get(index)
         const spandislike = spandislikeref.current.get(index)
         if(Number(spandislike.textContent)>0){
-            spandislike.textContent = spandislike.textContent - 1
+            spandislike.textContent = Number(spandislike.textContent) - 1
         }
-        spanlike.textContent = spanlike.textContent + 1
+        spanlike.textContent = Number(spanlike.textContent) + 1
         buttonlike.disabled = true
         buttondislike.disabled = false
         buttonlike.className = styles.likebuttongrey
