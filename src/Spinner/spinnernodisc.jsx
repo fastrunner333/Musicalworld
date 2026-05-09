9fcb1bd5f27290c5673a786b11b8db32085cbefa
musical <44aayushaa@gmail.com>
2026-05-10 01:21:56 +0530

    all bugs fixed

--- src/Spinner/spinnernodisc.jsx
+++ src/Spinner/spinnernodisc.jsx
@@ -1,8 +1,8 @@
 import styles from "./spinner.module.css"
 
 export function Spinnernodisc({displaysetting}){
-    return <div className={styles.back}>
+    return <>
             <div className={styles.spinner} style={{display:displaysetting}}></div>
             <div className={styles.loading} style={{display:displaysetting}}>Loading...</div>
-        </div>
+        </>
 }
