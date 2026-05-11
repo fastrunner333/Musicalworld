a86f8827889042b4945226e1cccb3f509f9217ae
musical <44aayushaa@gmail.com>
2026-05-11 11:06:00 +0530

    final build

--- src/Content/Post.jsx
+++ src/Content/Post.jsx
@@ -101,19 +101,19 @@ export function Post({filter}){
                                             console.log(typeof(index))
                                             if(post.liked){
                                                     setlikedisabled(likedisabled=>[...likedisabled, index])
-                                                    likebuttonvar = <button disabled={likedisabled.includes(Number(index))} data-id={post._id} className={styles.likebuttongrey} ref={(node)=>{node?likeref.current.set(index,node):likeref.current.delete(index)}} onClick={(e)=>sendlike(e,i)}></button>
+                                                    likebuttonvar = <button disabled={likedisabled.includes(Number(index))} checkarr={likedisabled} data-id={post._id} className={styles.likebuttongrey} ref={(node)=>{node?likeref.current.set(index,node):likeref.current.delete(index)}} onClick={(e)=>sendlike(e,i)}></button>
                                                 }
                                             else{
                                                     //setlikedisabled(...likedisabled, index)
-                                                    likebuttonvar =  <button disabled={likedisabled.includes(Number(index))} data-id={post._id} className={styles.likebutton} ref={(node)=>{node?likeref.current.set(index,node):likeref.current.delete(index)}} onClick={(e)=>sendlike(e,i)}></button>        
+                                                    likebuttonvar =  <button disabled={likedisabled.includes(Number(index))} checkarr={likedisabled} data-id={post._id} className={styles.likebutton} ref={(node)=>{node?likeref.current.set(index,node):likeref.current.delete(index)}} onClick={(e)=>sendlike(e,i)}></button>        
                                                 }
                                             if(post.disliked){
                                                     setdislikedisabled(dislikedisabled=>[...dislikedisabled, index])
-                                                    dislikebuttonvar = <button disabled={dislikedisabled.includes(Number(index))} data-id={post._id} className={styles.dislikebuttongrey} ref={(node)=>{node?dislikeref.current.set(index,node):dislikeref.current.delete(index)}} onClick={(e)=>senddislike(e,i)}></button>
+                                                    dislikebuttonvar = <button disabled={dislikedisabled.includes(Number(index))} checkarr={dislikedisabled} data-id={post._id} className={styles.dislikebuttongrey} ref={(node)=>{node?dislikeref.current.set(index,node):dislikeref.current.delete(index)}} onClick={(e)=>senddislike(e,i)}></button>
                                                 }
                                             else{
                                                    // setdislikedisabled(...dislikedisabled, index)
-                                                    dislikebuttonvar = <button disabled={dislikedisabled.includes(Number(index))} data-id={post._id} className={styles.dislikebutton} ref={(node)=>{node?dislikeref.current.set(index,node):dislikeref.current.delete(index)}} onClick={(e)=>senddislike(e,i)}></button>    
+                                                    dislikebuttonvar = <button disabled={dislikedisabled.includes(Number(index))} checkarr={dislikedisabled} data-id={post._id} className={styles.dislikebutton} ref={(node)=>{node?dislikeref.current.set(index,node):dislikeref.current.delete(index)}} onClick={(e)=>senddislike(e,i)}></button>    
                                                 }
                                             if(!post.mediatype){  
                                                     return  <div key={index} className={styles.userpostnomedia}>
