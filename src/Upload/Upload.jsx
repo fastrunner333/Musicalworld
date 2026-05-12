d6ffc897ecef26dfe670f9b9cdf70dfb14d06c16
musical <44aayushaa@gmail.com>
2026-05-12 11:55:01 +0530

    final build

--- src/Upload/Upload.jsx
+++ src/Upload/Upload.jsx
@@ -15,6 +15,7 @@ export function Upload(){
     const [musictype, setmusictype] = useState("")
     const {userToken} = useContext(USER)
     const [display, setdisplay] = useState("none")
+    const [parentdisplay, setparentdisplay] = useState(null)
     let validated = false
 
     function uploadbutton(e){
@@ -88,11 +89,12 @@ export function Upload(){
                         Select Filter
                     
                         <div style={parentdisplay} className={styles.dropdownlist}>
-                            <button className= {styles.dropdownbutton} type="button" onClick={()=>setmusictype("Classic")}>Classic</button>
-                            <button className= {styles.dropdownbutton} type="button" onClick={()=>setmusictype("Bollywood")}>Bollywood</button>
-                            <button className= {styles.dropdownbutton} type="button" onClick={()=>setmusictype("Western")}>Western</button>
-                            <button className= {styles.dropdownbutton} type="button" onClick={()=>setmusictype("Orchestra")}>Orchestra</button>
-                            <button className= {styles.dropdownbutton} type="button" onClick={()=>setmusictype("Nature Sounds")}>Nature Sounds</button>                    
+                            <button className= {styles.dropdownbutton} type="button" onClick={()=>{setmusictype("Classic") ;setparentdisplay({display:"none"});}}>Classic</button>
+
+                            <button className= {styles.dropdownbutton} type="button" onClick={()=>{setmusictype("Bollywood") ;setparentdisplay({display:"none"});}}>Bollywood</button>
+                            <button className= {styles.dropdownbutton} type="button" onClick={()=>{setmusictype("Western") ;setparentdisplay({display:"none"});}}>Western</button>
+                            <button className= {styles.dropdownbutton} type="button" onClick={()=>{setmusictype("Orchestra") ;setparentdisplay({display:"none"});}}>Orchestra</button>
+                            <button className= {styles.dropdownbutton} type="button" onClick={()=>{setmusictype("Nature Sounds") ;setparentdisplay({display:"none"});}}>Nature Sounds</button>                    
                         </div>
                     </div>
                     <div className={styles.attachmedia}>
