f93a07bd5d7c765e7b6a9cb0959a1ff585b3ba91
musical <44aayushaa@gmail.com>
2026-05-15 20:26:49 +0530

    changes

--- src/Spinner/spinner.jsx
+++ src/Spinner/spinner.jsx
@@ -5,9 +5,10 @@ export function Spinner(){
             <div className={styles.spinner} style={{display:"block"}}></div>
             <div className={styles.loading} style={{display:"block"}}>Loading...</div>
             <div className={styles.disclamer}>
-                    <div style={{color:"blue"}}>Servers are slow please be patient, website is not crashing, first request usually takes long, <div style={{color:"black"}}>refresh</div>if taking too long
-                    </div>
-                This website has option for making accounts disabled, to use the website use the provided username and password, this website is not for use, you are prohibited from making account on this website
+             <div className={styles.disclamer} style={discstyle}>
+                        <div style={{color:"blue"}}>Servers spin down with inactivity, please be patient, first request usually takes long, <div style={{color:"black"}}>refresh</div> if taking too long</div>
+                        To use the website use the username and password provided in resume, this website has option for making accounts disabled, this website is only for demo purposes, you are restricted from making account on this website
+             </div>
             </div>
         </div>
 }
