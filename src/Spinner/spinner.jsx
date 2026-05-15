4b33992eaf29927a826a4e77dcee762353ccc999
musical <44aayushaa@gmail.com>
2026-05-15 20:32:14 +0530

    style bugs fixed

--- src/Spinner/spinner.jsx
+++ src/Spinner/spinner.jsx
@@ -5,7 +5,7 @@ export function Spinner(){
             <div className={styles.spinner} style={{display:"block"}}></div>
             <div className={styles.loading} style={{display:"block"}}>Loading...</div>
             <div className={styles.disclamer}>
-             <div className={styles.disclamer} style={discstyle}>
+             <div className={styles.disclamer}>
                         <div style={{color:"blue"}}>Servers spin down with inactivity, please be patient, first request usually takes long, <div style={{color:"black"}}>refresh</div> if taking too long</div>
                         To use the website use the username and password provided in resume, this website has option for making accounts disabled, this website is only for demo purposes, you are restricted from making account on this website
              </div>
