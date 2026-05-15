72e98bc2fd81467fd51ea4205d40a26d15b0ea3a
musical <44aayushaa@gmail.com>
2026-05-16 00:31:45 +0530

    style bugs fixed

--- src/NoPage/Nopage.jsx
+++ src/NoPage/Nopage.jsx
@@ -1,10 +1,15 @@
 import styles from "./Nopage.module.css"
+import { useNavigate } from "react-router"
 
 export function Nopage(){
+
+    const navigate = useNavigate()
+
     return <div className={styles.back}>
     
         <div className={styles.number}>404</div>
         <div className={styles.text}>Page not found</div>
+        <button className={styles.button} onClick={()=>{navigate("/mainpage")}} >Go to Homepage</button>
 
     </div>
 }
